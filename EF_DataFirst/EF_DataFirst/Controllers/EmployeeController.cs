b205dd75d9e7239c004aaaa653c493959c6486cc
Ly111999 <39553327+Ly111999@users.noreply.github.com>
2018-11-10 22:33:05 +0700

    EF Database First

---
+++ EF_DataFirst/EF_DataFirst/Controllers/EmployeeController.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace EF_DataFirst.Controllers
+{
+    public class EmployeeController : Controller
+    {
+        EmployeeContext db = new EmployeeContext();
+        // GET: Employee
+        public ActionResult Index()
+        {
+            return View(db.Employees.ToList());
+        }
+
+        public ActionResult Edit()
+        {
+            throw new NotImplementedException();
+        }
+
+        public ActionResult Details()
+        {
+            throw new NotImplementedException();
+        }
+
+        public ActionResult Delete()
+        {
+            throw new NotImplementedException();
+        }
+
+        [HttpGet]
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Create(Employee employee)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Employees.Add(employee);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            return View(employee);
+          
+    }
+}
